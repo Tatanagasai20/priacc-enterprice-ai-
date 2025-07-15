ecd8375489e89b529c9eaf5886f17804090bdc46
Tatanagasai20 <142307963+Tatanagasai20@users.noreply.github.com>
2025-07-15 15:33:12 +0530

    Update index.js

--- frountend/src/index.js
+++ frountend/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';  // Optional, if you created index.css
+import './index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
